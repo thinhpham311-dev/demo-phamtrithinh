0c22039721e3b87a5792bbdc9060d259a101b7aa
thinhpham <phamthinh30522@gmail.com>
2023-12-26 20:02:06 -0800

    update code

--- src/components/template/Theme.js
+++ src/components/template/Theme.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { ConfigProvider } from 'components/ui'
-import useDarkMode from 'utils/hooks/useDarkMode'
 import { themeConfig } from 'configs/theme.config'
 
 const Theme = props => {
 
 	const theme = useSelector(state => state.theme)
-	const [ isDark ] = useDarkMode()
 
 	const currentTheme = {
-		mode: isDark ? 'dark' : 'light',
 		...themeConfig,
 		...theme
 	}
